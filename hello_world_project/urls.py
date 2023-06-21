91559cda1c9735de92268b1378414d7808872443
Jay Medina <jay.medina.celis@gmail.com>
2023-06-21 09:46:38 -0400

    Corrects placement of views and path in urls

--- hello_world_project/urls.py
+++ hello_world_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import home_view
+from hello_world_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_view),
+    path('home/', views.home_view),
 ]
